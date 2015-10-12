3979672e20babe3a6d72054061e7c00b3eca7a85
Todd Harrison <angrymonkeyofdoom@gmail.com>
2015-10-11 18:24:49 -0600

    Initial checkin.

---
+++ src/main/java/com/goodformentertainment/canary/bc/BCPlugin.java
@@ -0,0 +1,14 @@
+package com.goodformentertainment.canary.bc;
+
+import net.canarymod.plugin.Plugin;
+
+public class BCPlugin extends Plugin {
+    @Override
+    public boolean enable() {
+        return true;
+    }
+
+    @Override
+    public void disable() {
+    }
+}
